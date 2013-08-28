298f403ca557023cd6750082ef009a82fe37448e
Roelof Kemp <rkemp@cs.vu.nl>
2013-08-28 11:14:31 +0200

    Initial import

---
+++ gen/interdroid/swan/cuckoo_alarm_sensor/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package interdroid.swan.cuckoo_alarm_sensor;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
